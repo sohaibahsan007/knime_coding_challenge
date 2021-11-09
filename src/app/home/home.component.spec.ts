93a30eb3bb07c4ae51534ac1d317e838b28e12c8
Sohaib Ahsan <sohaibahsan007@live.com>
2021-11-10 02:33:44 +0500

    Test Beds Issue Resolved.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,6 @@
+import { NgSelectModule } from '@ng-select/ng-select';
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-
 import { SharedModule } from '@shared';
 import { HomeComponent } from './home.component';
 
@@ -11,7 +11,7 @@ describe('HomeComponent', () => {
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
-        imports: [SharedModule, HttpClientTestingModule],
+        imports: [SharedModule, NgSelectModule, HttpClientTestingModule],
         declarations: [HomeComponent],
       }).compileComponents();
     })
